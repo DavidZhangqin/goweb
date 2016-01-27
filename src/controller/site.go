5ba7de168b8a69e75c184bd5fa372c89bedcb12c
zhangqin <zhangqin@xiaomi.com>
2016-01-27 21:07:58 +0800

    orgin

--- src/controller/site.go
+++ src/controller/site.go
@@ -2,16 +2,16 @@ package controller
 
 import (
 	"fmt"
-	"log"
+	// "log"
 	"net/http"
 )
 
 func (*CtrlStr) Get_Site_Index(w http.ResponseWriter, r *http.Request) {
-	log.Println("site index")
+	// log.Println("site index")
 	fmt.Fprint(w, "get site/index\n")
 }
 
 func (*CtrlStr) Get_Site_Hello(w http.ResponseWriter, r *http.Request) {
-	log.Println("site hello")
-	fmt.Fprint(w, "get site/hello\n")
+	// log.Println("site hello")
+	fmt.Fprint(w, "hello world!\n")
 }
